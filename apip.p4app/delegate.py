6acbbae8f730362563d4343458743218aa815594
Grace Jia <gracejia25@gmail.com>
2021-05-16 14:54:05 -0400

    matching switch hdr defs

--- apip.p4app/delegate.py
+++ apip.p4app/delegate.py
@@ -21,12 +21,29 @@ class ApipFlag(Enum):
     VERIFY_RES = 4
     SHUTOFF = 5
 
-class Apip(Packet):
-   fields_desc = [BitField("flag", 0, 4),
-                    IPField("src", "127.0.0.1"),
-                    IPField("acc", "127.0.0.1")]
+class ApipFlag(Packet):
+   fields_desc = [BitField("flag", 0, 4)]
 
-bind_layers(Ether, Apip, type=0x87DD)
+class Apip(Packet):
+   fields_desc = [
+       BitField("accAddr", 0, 16),
+       BitField("retAddr", 0, 16),
+       IPField("dstAddr", "127.0.0.1")
+   ]
+
+class Brief(Packet):
+   fields_desc = [
+       BitField("host_id", 0, 48),
+       BitField("bloom", 0, 64)
+   ]
+
+class Verify(Packet):
+   fields_desc = [
+       BitField("fingerprint", 0, 64),
+       BitField("msg_auth", 0, 64)
+   ]
+
+bind_layers(Ether, ApipFlag, type=0x87DD)
 
 class Delegate(object):
     BRIEF_PERIOD = 30
@@ -63,29 +80,28 @@ class Delegate(object):
         if not isApip(pkt):
             return
         print_pkt(pkt[0][1])
+        flag = pkt[ApipFlag].flag
 
-        flag = pkt[Apip].flag
-        acc = pkt[Apip].acc
-        # TODO: how to extract
-        client_id = None
-        dst_ip = None
-        client_sid = None
-        dst_sid = None        
-        flow_id = (client_id, dst_ip, client_sid, dst_sid)
-
-        if flag == ApipFlag.BRIEF: # brief(pkt) = clientID || Fingerprint(pkt) || MAC_K_SD_S(clientID || Fingerprint(pkt))
-            # TODO: check client valid, extract bloom_filter
-            bloom_filter = None
-            self.briefs[flow_id].add(bloom_filter)
+        if flag == ApipFlag.BRIEF:
+            # TODO: check client valid (bootstrapping)
+            bloom_filter = pkt[Brief].bloom
+            self.briefs[pkt[Brief].host_id].add(bloom_filter)
             # TODO: set 30s timeout action
             return
         
         if flag == ApipFlag.VERIFY_REQ:
-            # 1. Check delegate has received a brief from S containing Fingerprint(pkt)
-            if bloom_filter not in self.briefs[client_id]:
+            # 1. Check delegate has received a brief from client containing Fingerprint(pkt)
+            fingerprint = pkt[Verify].fingerprint
+            client_id = None
+            for k,v in self.briefs:
+                if fingerprint in v:
+                    client_id = k
+                    
+            if client_id is None:
                 self.send_drop_flow(pkt)
                 return
-            # 2. Check accAddr in pkt is using an SID assigned to S
+            # 2. Check accAddr in pkt is using an SID assigned to client
+            # TODO: get client_sid
             if client_sid not in self.assigned_sids[client_id]:
                 self.send_drop_flow(pkt)
                 return
@@ -96,7 +112,8 @@ class Delegate(object):
 
             # Return copy of the verification packet signed with private key to verifier V (V will add S -> R to its whitelist).
             # same fingerprint, key = something
-            resp = Apip(flag=pkt[Apip].flag, dst=pkt[Apip].src, acc=pkt[Apip].acc)
+            resp = Verify(fingerprint=pkt[Verify].fingerprint, msg_auth=pkt[Verify].msg_auth)
+            resp = ApipFlag(flag=pkt[ApipFlag].flag) / resp
             resp = Ether(src=get_if_hwaddr(iface), dst=pkt[Ether].src) / resp
             send(resp, verbose=False)
 
